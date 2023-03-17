45f11ee608962909ef0d8eb41a358783fef65d6a
aliyu <1324149139@qq.com>
2023-03-17 12:50:09 +0800

    wip" 二次封装 element 组件

---
+++ src/components/login/panel-common-content.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center items-center">
+    <el-icon>
+      <slot name="icon"></slot>
+    </el-icon>
+    <span class="ml-1">
+      <slot name="label"></slot>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/components/login/panel-form-cpn.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-form
+    :model="account"
+    ref="formRef"
+    label-width="60px"
+    :rules="accountRules"
+    :hide-required-asterisk="isHideasterisk"
+  >
+    <el-form-item :prop="itemProp" :label="itemName">
+      <el-input v-model="account.name" />
+    </el-form-item>
+    <el-form-item :prop="itemProp" :label="itemPassword">
+      <el-input v-model="account.password" :type="inputType" />
+      <el-button type="primary" :size="small" v-show="isShowVerificationCode"
+        >获取验证码
+      </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup lang="ts">
+import type { FormRules } from 'element-plus';
+
+defineProps<{
+  account: {
+    name: string;
+    password: string;
+  };
+  accountRules: FormRules;
+  isHideasterisk: boolean;
+  itemName: string;
+  itemPassword: string;
+  itemProp: string;
+  inputType: string;
+  small: 'large' | 'default' | 'small';
+  isShowVerificationCode: boolean;
+}>();
+</script>
+
+<style scoped></style>
